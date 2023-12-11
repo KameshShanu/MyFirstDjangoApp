80a6a66db4da2ce8a85e57266019c0f3d8313d0a
Kamesh Ganegama <shanuuuk@gmail.com>
2023-12-11 10:42:40 -0800

    Made some changes on fields.

--- teammember/admin.py
+++ teammember/admin.py
@@ -4,4 +4,4 @@ from .models import Teammember
 # Register your models here.
 @admin.register(Teammember)
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',), }
+    prepopulated_fields = {'firstName': ('firstName',), }
--- teammember/forms.py
+++ teammember/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Teammember
 
+NUMS = [('Regular','Admin')]
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Teammember
         fields = "__all__"
+        NUMS = forms.ChoiceField(widget=forms.RadioSelect(choices=NUMS))
